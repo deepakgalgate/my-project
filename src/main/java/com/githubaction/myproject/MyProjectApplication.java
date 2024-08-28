d38ce1f37944203e08e4f6417a6bffc1adc1c6b7
deepak galgate <deepakgalgate19294@gmail.com>
2024-08-29 00:03:51 +0530

    dockerfile and restcontroller changes.

--- src/main/java/com/githubaction/myproject/MyProjectApplication.java
+++ src/main/java/com/githubaction/myproject/MyProjectApplication.java
@@ -2,9 +2,17 @@ package com.githubaction.myproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MyProjectApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyProjectApplication.class, args);
